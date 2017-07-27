879a3e5212f0f0adefe29f821a978d9a593508d1
Tada Matz <matz@west.sd.keio.ac.jp>
2017-07-27 16:54:21 +0900

    making file designator

--- GCTC_GUI/start.sh
+++ GCTC_GUI/start.sh
@@ -1,3 +1,7 @@
-export JSON_FILE_DIR="./public/jsons"
+if [ -z "${JSON_FILE_DIR+x}" ] ; then #if JSON_FILE_DIR is not defined, define.
+  echo "JSON_FILE_DIR was not defined before, define JSON_FILE_DIR"
+  export JSON_FILE_DIR="./public/jsons"
+fi
+echo $JSON_FILE_DIR
 export PORT=3333
-npm start
+#npm start
--- docker/docker.sh
+++ docker/docker.sh
@@ -1,4 +1,7 @@
-PORT=3333
+GUI_PORT=5000
+JSON_DIR="../GCTC_GUI/public/jsons" #relative path allowed
+JSON_DIR=$(cd $(dirname $0) && cd $JSON_DIR && pwd) #convert to absolute path
 sudo docker build -t gctcgui `pwd`
-sudo docker run -it -p $PORT:3333 --rm gctcgui /bin/bash ./start.sh
+#sudo docker run -it -v $JSON_DIR:/jsons -p $GUI_PORT:3333 --rm gctcgui /bin/bash 
+sudo docker run -it -v $JSON_DIR:/jsons -p $GUI_PORT:3333 --rm gctcgui /bin/bash ./start.sh
 
