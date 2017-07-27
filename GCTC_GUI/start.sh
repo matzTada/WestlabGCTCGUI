caa1539d7d43ed035ef2afbced9f8ee3a0067cd6
Tada Matz <matz@west.sd.keio.ac.jp>
2017-07-28 00:40:33 +0900

    modify directory name

--- GCTC_GUI/start.sh
+++ GCTC_GUI/start.sh
@@ -6,7 +6,7 @@ echo "PORT: $PORT"
 
 if [ -z "${JSON_FILE_DIR+x}" ] ; then #if JSON_FILE_DIR is not defined, define.
   echo "JSON_FILE_DIR was not defined before, define JSON_FILE_DIR"
-  export JSON_FILE_DIR="./public/jsons"　#change here if you start express app without Docker
+  export JSON_FILE_DIR="./public/jsons" #change here if you start express app without Docker
 fi
 echo "JSON_FILE_DIR: $JSON_FILE_DIR"
 
