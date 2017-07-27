3c86578673bf67d909c14b2a504471bc636d7b68
Tada Matz <matz@west.sd.keio.ac.jp>
2017-07-27 17:49:45 +0900

    succeed in docker

--- docker/docker.sh
+++
@@ -1,7 +0,0 @@
-GUI_PORT=5000
-JSON_DIR="../GCTC_GUI/public/jsons" #relative path allowed
-JSON_DIR=$(cd $(dirname $0) && cd $JSON_DIR && pwd) #convert to absolute path
-sudo docker build -t gctcgui `pwd`
-#sudo docker run -it -v $JSON_DIR:/jsons -p $GUI_PORT:3333 --rm gctcgui /bin/bash 
-sudo docker run -it -v $JSON_DIR:/jsons -p $GUI_PORT:3333 --rm gctcgui /bin/bash ./start.sh
-
