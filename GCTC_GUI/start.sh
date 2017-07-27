604577978347edc37f04f2c80da4513f57c70160
Tada Matz <matz@west.sd.keio.ac.jp>
2017-07-27 17:03:23 +0900

    tried variable change

--- GCTC_GUI/start.sh
+++ GCTC_GUI/start.sh
@@ -2,6 +2,6 @@ if [ -z "${JSON_FILE_DIR+x}" ] ; then #if JSON_FILE_DIR is not defined, define.
   echo "JSON_FILE_DIR was not defined before, define JSON_FILE_DIR"
   export JSON_FILE_DIR="./public/jsons"
 fi
-echo $JSON_FILE_DIR
+echo "JSON_FILE_DIR: $JSON_FILE_DIR"
 export PORT=3333
-#npm start
+npm start
