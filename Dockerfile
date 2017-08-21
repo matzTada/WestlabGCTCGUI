4f80c958e3d7354c6c48b3ec5ae722faf4bb82b7
Tada Matz <matz@west.sd.keio.ac.jp>
2017-08-21 12:47:32 +0900

    localize docker component

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM node
 #for raspberry pi
 #FROM hypriot/rpi-node
 MAINTAINER matzTada
-RUN ["git", "clone", "https://github.com/matzTada/WestlabGCTCGUI"]
+#RUN ["git", "clone", "https://github.com/matzTada/WestlabGCTCGUI"]
+ADD . /WestlabGCTCGUI
 WORKDIR /WestlabGCTCGUI/GCTC_GUI
 ENV JSON_FILE_DIR /jsons
 ENV PORT 3333
--- docker/launchContainer.sh
+++ docker/launchContainer.sh
@@ -2,7 +2,7 @@
 sudo docker build --no-cache=true -t gctcgui `pwd`
 
 #run docker container
-GUI_PORT=3333
+GUI_PORT=3001
 JSON_DIR="../config_jsons" #relative path allowed. change here for config json files
 #JSON_DIR="../GCTC_GUI/public/jsons" #relative path allowed. change here for config json files
 JSON_DIR=$(cd $(dirname $0) && cd $JSON_DIR && pwd) #convert to absolute path
